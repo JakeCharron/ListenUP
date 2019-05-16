b071e54f5a68fa2e32cc54e33e67e5c56c184dfe
Jake Charron <jcharron0306@gmail.com>
2019-05-16 03:24:33 -0400

    Grind==done
    hamburger

--- ListenUPCsFinal/app/src/main/java/com/jcpacks/listenupcsfinal/DiagnosticActivity.java
+++ ListenUPCsFinal/app/src/main/java/com/jcpacks/listenupcsfinal/DiagnosticActivity.java
@@ -3,12 +3,14 @@ package com.jcpacks.listenupcsfinal;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class DiagnosticActivity extends AppCompatActivity {
 
     Intent intent;
-    String Time;
-    String Age;
+    String ActualAge;
+    String HearingAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +18,35 @@ public class DiagnosticActivity extends AppCompatActivity {
         setContentView(R.layout.activity_diagnostic);
 
         intent = getIntent();
-        Age = intent.getStringExtra("Age");
-        Time = intent.getStringExtra("Time");
+        ActualAge = intent.getStringExtra("ActualAge");
+        HearingAge = intent.getStringExtra("HearingAge");
+
+        Log.d("Age Value: ", ActualAge);
+        Log.d("Hearing Age: ", HearingAge);
+
+        setContentView(R.layout.activity_diagnostic);
+
+        TextView heatingAgeLabel = findViewById(R.id.hearingAge);
+        heatingAgeLabel.setText("Hearing Age: " + HearingAge);
+
+        TextView AgeDif = findViewById(R.id.AgeDifference);
+//        try{
+//            int actAge = Integer.parseInt(ActualAge);
+//            int HeaAge = Integer.parseInt(HearingAge);
+//            int difference = actAge-HeaAge;
+//            if(difference==0){
+//                AgeDif.setText("Your hearing age is the same as your actual age!");
+//            }else{
+//            }
+//        }catch(NumberFormatException e){
+//
+//        }
+
+
+
+
     }
+
 
 
 
--- ListenUPCsFinal/app/src/main/java/com/jcpacks/listenupcsfinal/TestActivity.java
+++ ListenUPCsFinal/app/src/main/java/com/jcpacks/listenupcsfinal/TestActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.System.err;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -22,6 +26,7 @@ public class TestActivity extends AppCompatActivity {
         AudioTestButton = (Button)findViewById(R.id.AudioTestButton);
     }
 
+    MediaPlayer mediaPlayer;
 
     public void TestAction(View view){
 
@@ -29,16 +34,102 @@ public class TestActivity extends AppCompatActivity {
             started = true;
             AudioTestButton.setText("Stop");
             stopwatch.start();
-            MediaPlayer mediaPlayer= MediaPlayer.create(this, R.raw.sounds);
+            mediaPlayer= MediaPlayer.create(this, R.raw.sounds);
             mediaPlayer.start();
         }else{
             stopwatch.stop();
+            mediaPlayer.stop();
+
+            Log.d("result", Long.toString(stopwatch.getElapsedTimeSecs()));
+            String seconds = Long.toString(stopwatch.getElapsedTimeSecs());
+            int integerSeconds = 0;
+            try{
+                int realSeconds = Integer.parseInt(seconds);
+                Log.d("Real Seconds" ,Integer.toString(realSeconds));
+                integerSeconds = realSeconds;
+            }catch (NumberFormatException e){
+                Log.d("NumberFormatException", "Yeet delete");
+            }
+            String spegget = Integer.toString(secToHz(integerSeconds));
+
+
+            Log.d("Herts", spegget );
+
+            Log.d("Hearing Age", Integer.toString(secToAge(integerSeconds)));
+
             Age = (EditText)findViewById(R.id.AgeInput);
+            String value = Age.getText().toString();
+            int actualAge = 0;
+            try{
+                actualAge = Integer.parseInt(value);
+                Log.d("Actual Age", Integer.toString(actualAge));
+
+            }catch (NumberFormatException e){
+                Log.d("AgeIn", value);
+            }
             Intent intent = new Intent(this, DiagnosticActivity.class);
-            intent.putExtra("Time", stopwatch.getElapsedTimeSecs());
-            intent.putExtra("Age", Age.getText().toString() );
+            intent.putExtra("ActualAge", Integer.toString(actualAge));
+            intent.putExtra("HearingAge", Integer.toString(secToAge(integerSeconds)));
+
             startActivity(intent);
+
         }
 
     }
+
+    public static int secToHz( int  s){
+        if(s <= 4){
+            return 500;
+        } else if (s <= 8 && s >4){
+            return 750;
+        }else if (s <= 12 && s >8){
+            return 1000;
+        }else if (s <= 16 && s >12){
+            return 1500;
+        }else if (s <= 20 && s >16){
+            return 2000;
+        }else if (s <= 24 && s >20){
+            return 3000;
+        }else if (s <= 28 && s >24){
+            return 4000;
+        }else if (s <= 32 && s >28){
+            return 6000;
+        }else if (s <= 36 && s >32){
+            return 8000;
+        }else if (s <= 40 && s >36){
+            return 10000;
+        }else if (s <= 44 && s >40){
+            return 12000;
+        }else if (s <= 48 && s >44){
+            return 14000;
+        }else if (s <= 52 && s >48){
+            return 16000;
+        }else if (s >52){
+            return 18000;
+        }else{
+            return 0;
+        }
+    }
+
+    public static int secToAge(int seconds ){
+
+        int hz = secToHz( seconds);
+
+        int a = 0;
+        if(hz>4000){
+            a = 2;
+        }
+        if (hz > 4000 && hz <= 10000){
+            a = 4;
+        }
+        if (hz>10000){
+            a = 7;
+        }
+        return(112-(hz/250)-a-((hz/250)/10));
+    }
+
+
+
+
+
 }
